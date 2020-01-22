6b084281f5bb8c8276a8e34946a1699d2a8cc796
TMats <t.matsu.changmin@gmail.com>
2020-01-22 18:44:06 +0900

    custom gym

--- setups/setup.sh
+++ setups/setup.sh
@@ -1,6 +1,8 @@
 pip install -U pip
 pip install -U setuptools
 pip install -r requirements.txt
+git clone -b change_reward https://github.com/frt03/gym.git
+pip install -e 'gym[all]'
 Xvfb :0 -screen 0 1400x900x24 &
 x11vnc -display :0 -passwd pass -forever &
 echo 'export DISPLAY=:0' >> /root/.zshrc
